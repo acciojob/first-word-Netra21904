ec72756eae4146904daca668c5a6a0e704a5ee18
Netra21904 <128806086+Netra21904@users.noreply.github.com>
2025-02-06 13:01:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function firstWord(s) {
-  // your code here
+  if (s === null || s.length === 0) {
+    return s;
+  }
+
+  const firstSpaceIndex = s.indexOf(' ');
+
+  if (firstSpaceIndex === -1) {
+    return s;
+  } else {
+    return s.substring(0, firstSpaceIndex);
+  }
 }
-
-// Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
